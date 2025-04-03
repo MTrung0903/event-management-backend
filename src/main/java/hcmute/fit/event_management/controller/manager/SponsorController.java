77130a90679eedf31d6a28196c117e657388b58d
Tungdever <sidedlove03@gmail.com>
2025-04-03 21:06:37 +0700

    api

--- src/main/java/hcmute/fit/event_management/controller/manager/SponsorController.java
+++ src/main/java/hcmute/fit/event_management/controller/manager/SponsorController.java
@@ -1,5 +1,7 @@
 package hcmute.fit.event_management.controller.manager;
 
+import com.cloudinary.Cloudinary;
+import hcmute.fit.event_management.CloudinaryConfig;
 import hcmute.fit.event_management.dto.SponsorDTO;
 import hcmute.fit.event_management.dto.SponsorEventDTO;
 import hcmute.fit.event_management.dto.SponsorShipDTO;
@@ -11,6 +13,7 @@ import hcmute.fit.event_management.service.IEventService;
 import hcmute.fit.event_management.service.ISponsorEventService;
 import hcmute.fit.event_management.service.ISponsorService;
 
+import hcmute.fit.event_management.service.Impl.CloudinaryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,8 +22,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import payload.Response;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -31,6 +36,8 @@ public class SponsorController {
     ISponsorEventService sponsorEventService;
     @Autowired
     IEventService eventService;
+    @Autowired
+    CloudinaryService cloudinaryService;
     @GetMapping("/myevent/sponsor")
     public ResponseEntity<?> getSponsorsByEventId(@RequestParam("eid") int eid) {
         List<SponsorEvent> sponsorEvents = sponsorEventService.findByEventId(eid);
@@ -41,7 +48,7 @@ public class SponsorController {
             sponsorEventDTO.setSponsorName(sponsorEvent.getSponsor().getSponsorName());
             sponsorEventDTO.setSponsorEmail(sponsorEvent.getSponsor().getSponsorEmail());
             sponsorEventDTO.setSponsorAddress(sponsorEvent.getSponsor().getSponsorAddress());
-            sponsorEventDTO.setSponsorLogo(sponsorEvent.getSponsor().getSponsorLogo());
+            sponsorEventDTO.setSponsorLogo(cloudinaryService.getFileUrl(sponsorEvent.getSponsor().getSponsorLogo()));
             sponsorEventDTO.setSponsorPhone(sponsorEvent.getSponsor().getSponsorPhone());
             sponsorEventDTO.setSponsorWebsite(sponsorEvent.getSponsor().getSponsorWebsite());
             sponsorEventDTO.setSponsorRepresentativeName(sponsorEvent.getSponsor().getSponsorRepresentativeName());
@@ -62,27 +69,58 @@ public class SponsorController {
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
     @PostMapping("/myevent/sponsor")
-    public ResponseEntity<?> createSponsorByEventId(@RequestParam("eid") int eid ,@RequestBody SponsorEventDTO sponsorEventDTO) {
-        Sponsor sponsor = new Sponsor();
+    public ResponseEntity<?> createSponsorByEventId(@RequestParam("eid") int eid,@ModelAttribute SponsorEventDTO sponsorEventDTO, // Nhận toàn bộ dữ liệu dạng text
+                                                    @RequestParam(value = "sponsorLogoFile", required = false) MultipartFile sponsorLogoFile,
+                                                    @RequestParam(value = "sponsorContractFile", required = false) MultipartFile sponsorContract) throws IOException {
+        System.out.println("Nhận sponsorLogoFile: " + (sponsorLogoFile != null ? sponsorLogoFile.getOriginalFilename() : "Không có file"));
+        System.out.println("Nhận sponsorContractFile: " + (sponsorContract != null ? sponsorContract.getOriginalFilename() : "Không có file"));
+        Sponsor sponsor = sponsorService.findById(sponsorEventDTO.getSponsorId()).orElse(new Sponsor());
+        String sponsorLogoUrl = null;
+        String sponsorContractUrl = null;
         BeanUtils.copyProperties(sponsorEventDTO, sponsor);
+        System.out.println("============" + sponsorEventDTO.getSponsorLogo() + "============");
+        if (sponsorLogoFile != null && !sponsorLogoFile.isEmpty()) {
+            sponsorLogoUrl = cloudinaryService.uploadFile(sponsorLogoFile);
+        }
+        if (sponsorContract != null && !sponsorContract.isEmpty()) {
+            sponsorContractUrl = cloudinaryService.uploadFile(sponsorContract);
+        }
+        sponsor.setSponsorLogo(sponsorLogoUrl);
         sponsor = sponsorService.save(sponsor);
         SponsorEvent sponsorEvent = new SponsorEvent();
         BeanUtils.copyProperties(sponsorEventDTO, sponsorEvent);
-        sponsorEvent.setSponsor(sponsor);
-        sponsorEvent.setEvent(eventService.findById(eid).orElse(new Event()));
+        sponsorEvent.setSponsorContract(sponsorContractUrl);
+        SponsorEventId sponsorEventId = new SponsorEventId();
+        sponsorEventId.setSponsorId(sponsor.getSponsorId());
+        sponsorEventId.setEventId(eid);
+        sponsorEvent.setId(sponsorEventId);
         sponsorEventService.save(sponsorEvent);
         Response response = new Response(200,"",null);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
     @PutMapping("/myevent/sponsor")
-    public ResponseEntity<?> updateSponsorByEventId(@RequestParam("eid") int eid, @RequestBody SponsorEventDTO sponsorEventDTO) {
+    public ResponseEntity<?> updateSponsorByEventId(@RequestParam("eid") int eid, @ModelAttribute SponsorEventDTO sponsorEventDTO, // Nhận toàn bộ dữ liệu dạng text
+                                                    @RequestParam(value = "sponsorLogoFile", required = false) MultipartFile sponsorLogoFile,
+                                                    @RequestParam(value = "sponsorContractFile", required = false) MultipartFile sponsorContract) throws IOException {
         Sponsor sponsor = new Sponsor();
-        BeanUtils.copyProperties(sponsorEventDTO, sponsor);
-        sponsor = sponsorService.save(sponsor);
         SponsorEvent sponsorEvent = new SponsorEvent();
+        String sponsorLogoUrl = null;
+        String sponsorContractUrl = null;
+        BeanUtils.copyProperties(sponsorEventDTO, sponsor);
+        if (sponsorLogoFile != null && !sponsorLogoFile.isEmpty()) {
+            sponsorLogoUrl = cloudinaryService.uploadFile(sponsorLogoFile);
+            sponsor.setSponsorLogo(sponsorLogoUrl);
+        }
+        if (sponsorContract != null && !sponsorContract.isEmpty()) {
+            sponsorContractUrl = cloudinaryService.uploadFile(sponsorContract);
+            sponsorEvent.setSponsorContract(sponsorContractUrl);
+        }
+        sponsor = sponsorService.save(sponsor);
         BeanUtils.copyProperties(sponsorEventDTO, sponsorEvent);
-        sponsorEvent.setSponsor(sponsor);
-        sponsorEvent.setEvent(eventService.findById(eid).orElse(new Event()));
+        SponsorEventId sponsorEventId = new SponsorEventId();
+        sponsorEventId.setSponsorId(sponsor.getSponsorId());
+        sponsorEventId.setEventId(eid);
+        sponsorEvent.setId(sponsorEventId);
         sponsorEventService.save(sponsorEvent);
         Response response = new Response(200,"",null);
         return new ResponseEntity<>(response,HttpStatus.OK);
